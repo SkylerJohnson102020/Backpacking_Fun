a1dd52c02b92c5244537eeb22d956c2126d984a4
SkylerJohnson102020 <basseuph@gmail.com>
2021-02-24 15:39:49 -0800

    1st js add

---
+++ app1.js
@@ -0,0 +1,13 @@
+var today = new Date();
+var hourNow = today.getHours();
+ver greeting;
+
+if (hourNow > 18) {
+    greetng = 'Good Evening!';
+} else if (hourNow > 12) {
+    greeting = 'Good Afternoon!';
+} else if (hourNow > 0) {
+    greeting = 'Good Morning!';
+} else {
+    greeting = 'Welcome!';
+}
