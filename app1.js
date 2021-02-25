8ffadc1e915345314efc3c5cfbb291eeacf8e52a
SkylerJohnson102020 <basseuph@gmail.com>
2021-02-25 12:43:44 -0800

    js edits

--- app1.js
+++ app1.js
@@ -1,7 +1,9 @@
-let today = new Date();
-let hourNow = today.getHours();
-let greeting;
-console.log(hourNow);
+function getDate(){
+    let today = new Date();
+    let hourNow = today.getHours();
+    let greeting;
+
+    console.log(hourNow);
 if (hourNow > 18) {
     greeting = 'Good Evening!';
 } else if (hourNow > 12) {
@@ -11,6 +13,23 @@ if (hourNow > 18) {
 } else {
     greeting = 'Welcome!';
 }
-
 document.write(greeting);
 console.log(greeting);
+}
+
+function getUserName(){
+    let userName = prompt('Please Enter your Name: ');
+    document.write('<h2>' + userName + '</h2>')
+    return userName;
+}
+console.log(userName);
+
+function specialMessage(){
+    if (userName == 'Skyler'){
+        document.write('Happy Trails, Skyler');
+    } else {
+        document.write('Rise and Climb, ' + userName);
+    }
+}
+specialMessage();
+console.log()
